2bdccb0d1c8ba703d8ebfa4a6865f4b6559c1ca5
kmhswimgirl <163170240+kmhswimgirl@users.noreply.github.com>
2025-07-14 23:18:47 -0400

    began mapping out smooth drive

--- lab_2/smooth_drive.py
+++ lab_2/smooth_drive.py
@@ -1,2 +1,13 @@
 # for lab2 extra credit smooth drive
 
+"""
+PID controller
+
+Output = (Proportional Gain * Error) + 
+        (Integral Gain * Integral of Error) + 
+        (Derivative Gain * Derivative of Error)
+
+error = target - current
+
+for this particular purpose, euclidian distance to target is what is used for "distance remaining" or error
+"""
